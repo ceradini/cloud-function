fa6264a27decfb363bb6d394180ac6deb7808cd7
Matteo Ceradini <matteo.ceradini@gmail.com>
2018-03-27 11:22:10 +0200

    up

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 exports.testNLP = (event, callback) => {
-	console.log('here we are');
+	const language  = require('@google-cloud/language');
 
-	callback.send('fuck');
+	const language_client = new language.LanguageServiceClient();
+
+	// The text to analyze
+	var text = 'Hello, world!';
+
+	const document = {
+	  content: text,
+	  type: 'PLAIN_TEXT',
+	};
+
+	// Detects the sentiment of the text
+	language_client
+	.analyzeSentiment({document: document})
+	.then(results => {
+		const sentiment = results[0].documentSentiment;
+
+		console.log(`Text: ${text}`);
+		console.log(`Sentiment score: ${sentiment.score}`);
+		console.log(`Sentiment magnitude: ${sentiment.magnitude}`);
+	})
+	.catch(err => {
+		console.error('ERROR:', err);
+	});
+
+	callback.send('END');
 };
